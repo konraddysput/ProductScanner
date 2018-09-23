445d4014b67564e836f67cc5a55091998e89192e
Konrad Dysput <konrad.dysput@gmail.com>
2018-09-24 00:41:22 +0200

    Full ontology from protege + jena API

--- src/ProductScanner.WebSemantic/src/main/java/com/productScanner/Main.java
+++ src/ProductScanner.WebSemantic/src/main/java/com/productScanner/Main.java
@@ -2,13 +2,10 @@ package com.productScanner;
 
 import com.productScanner.model.ImageClasificationEventResultEntry;
 import com.productScanner.model.ImagePreprocessingEvent;
-import com.productScanner.semanticModel.Product;
 import com.productScanner.services.WebSemanticService;
-import org.apache.jena.rdf.model.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 public class Main {
@@ -19,26 +16,27 @@ public class Main {
 
         WebSemanticService service=  new WebSemanticService();
         ImagePreprocessingEvent event= new ImagePreprocessingEvent();
-        event.Id = 1;
+        event.Id = 12412;
 
-        List<ImageClasificationEventResultEntry> data = new ArrayList<>();
+        ArrayList<ImageClasificationEventResultEntry> data = new ArrayList<>();
         ImageClasificationEventResultEntry pepsi = new ImageClasificationEventResultEntry();
-        pepsi.Category = "papsi";
-        pepsi.Id = 1;
+        pepsi.Category = "pepsi";
+        pepsi.Id = 123;
         pepsi.Position= new double[] {0.12,0.124124, 0.62464, 0.7573234};
         pepsi.Score =1;
 
         ImageClasificationEventResultEntry cola = new ImageClasificationEventResultEntry();
-        pepsi.Category = "cola";
-        pepsi.Id = 2;
-        pepsi.Position= new double[] {0.12,0.124124, 0.62464, 0.7573234};
-        pepsi.Score =0.95;
+        cola.Category = "cola";
+        cola.Id = 312;
+        cola.Position= new double[] {0.12,0.124124, 0.62464, 0.7573234};
+        cola.Score =0.95;
 
         data.add(pepsi);
         data.add(cola);
         event.Entries =data.toArray(new ImageClasificationEventResultEntry[data.size()]);
 
-        service.findInvalidPositions(event);
-
+        service.addToOntology(data, event.Id);
+        service.startResoing();
+        System.out.println(service.getResult(data));
     }
 }
